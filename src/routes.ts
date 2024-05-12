57ecf4671b12370a1516cc96e43f222b94c879fe
Joseph Infante <josephinfante.y@gmail.com>
2024-05-12 16:07:32 -0500

    adding user and auth routes to app router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import { AuthRouter, UsersRouter } from './api'
 
 export class AppRoutes {
 	static get routes(): Router {
 		const router = Router()
+
+		router.use('/api/v1/auth', AuthRouter.routes)
+		router.use('/api/v1/user', UsersRouter.routes)
 
 		return router
 	}
