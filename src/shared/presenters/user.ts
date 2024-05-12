2f29cb901908622bcf6e433a553faaf07eb6c266
Joseph Infante <josephinfante.y@gmail.com>
2024-05-12 16:06:10 -0500

    adding presenters

---
+++ src/shared/presenters/user.ts
@@ -0,0 +1,21 @@
+import { Users } from '../classes/users.class'
+
+interface UserPresenter {
+	id: string
+	first_name: string
+	last_name: string
+	email: string
+	authentication_type: string
+	is_verified: boolean
+}
+
+export function userPresenter(data: Users): UserPresenter {
+	return {
+		id: data.id,
+		first_name: data.first_name,
+		last_name: data.last_name,
+		email: data.email,
+		authentication_type: data.authentication_type,
+		is_verified: data.is_verified,
+	}
+}
