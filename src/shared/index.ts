e405bf2e515db2b0ab6935d660dd4c79cc9aea78
Joseph Infante <josephinfante.y@gmail.com>
2024-05-12 21:00:14 -0500

    updating barels

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './classes/access.class'
+export * from './classes/properties.class'
 export * from './classes/users.class'
 export { default as redis } from './connections/database/redis'
 export * from './connections/database/sequelize'
