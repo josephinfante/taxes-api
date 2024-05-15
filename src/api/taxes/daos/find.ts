aa8e9a91099e32ec26657442a7dc9830dcbb5704
Joseph Infante <josephinfante.y@gmail.com>
2024-05-14 22:00:58 -0500

    updating methods to use new model structure

---
+++ src/api/taxes/daos/find.ts
@@ -0,0 +1,16 @@
+import { TaxesModel } from '../../../models'
+import { CustomError, Taxes } from '../../../shared'
+
+export async function findTax(id: string): Promise<Taxes> {
+	try {
+		if (!id) throw CustomError.badRequest('El ID es requerido.')
+
+		const tax_exists = await TaxesModel.findByPk(id)
+		if (!tax_exists) throw CustomError.badRequest(`El impuesto con ID '${id}' no existe.`)
+
+		return tax_exists.dataValues
+	} catch (error) {
+		if (error instanceof CustomError) throw error
+		throw CustomError.internal()
+	}
+}
