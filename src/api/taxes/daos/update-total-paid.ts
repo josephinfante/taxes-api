92e82c6c4a6ebb62149e040410c7a7a43957e71b
Joseph Infante <josephinfante.y@gmail.com>
2024-05-14 22:01:05 -0500

    adding new method for taxes

---
+++ src/api/taxes/daos/update-total-paid.ts
@@ -0,0 +1,40 @@
+import { TAXES_STATUS } from '../../../config'
+import { PaymentsModel, TaxesModel } from '../../../models'
+import { CustomError } from '../../../shared'
+
+export async function updateTaxTotalPaid(id: string): Promise<void> {
+	try {
+		if (!id) throw CustomError.badRequest('El ID es requerido.')
+
+		const tax_exists = await TaxesModel.findByPk(id, {
+			include: [{ model: PaymentsModel, attributes: ['total_paid_amount'] }],
+		})
+		if (!tax_exists) throw CustomError.badRequest(`El impuesto con ID '${id}' no existe.`)
+
+		let total_paid_amount = 0
+		if (tax_exists.dataValues.payments) {
+			total_paid_amount = tax_exists.dataValues.payments.reduce((acc: number, payment: any) => {
+				return acc + payment.total_paid_amount
+			}, 0)
+		}
+
+		if (tax_exists.dataValues.total_paid_amount == Number(total_paid_amount)) {
+			tax_exists.set({
+				status: TAXES_STATUS.PAID,
+				updated_at: Date.now(),
+			})
+		} else {
+			tax_exists.set({
+				status: TAXES_STATUS.PENDING,
+				total_paid_amount: total_paid_amount,
+				updated_at: Date.now(),
+			})
+		}
+
+		console.log(total_paid_amount)
+		await tax_exists.save()
+	} catch (error) {
+		if (error instanceof CustomError) throw error
+		throw CustomError.internal()
+	}
+}
